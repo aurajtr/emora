dccbe357c86042a25730d3a68e60faf23c9cfedd
Aura Jatra <aurajatra9@gmail.com>
2026-06-17 21:16:48 +0700

    Initial Commit

---
+++ emora/emoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  emoraApp.swift
+//  emora
+//
+//  Created by Aura Jatra on 17/06/26.
+//
+
+import SwiftUI
+
+@main
+struct emoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
